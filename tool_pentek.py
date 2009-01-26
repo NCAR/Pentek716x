fe8b7ee33ef89629610a181df36ec0a5ae57ca06
Charles Martin <martinc@ucar.edu>
2009-01-26 22:21:48 +0000

    Cleaning up doxygen and SConscripts.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/pentek@6780 da89c675-a4d7-0310-8a15-9e7546503515

--- tool_pentek.py
+++ tool_pentek.py
@@ -5,6 +5,7 @@ PENTEK_ROOT = os.environ['PENTEK_ROOT']
 PENTEK_INCLUDE = PENTEK_ROOT+'/include'
 
 tools = Split("""
+doxygen
 """)
 
 env = Environment(tools = ['default'] + tools)
@@ -19,9 +20,18 @@ p7142.cpp
 p7142hcr.cpp
 """)
 
+headers = Split("""
+p7140.h
+p7142.h
+p7142hcr.h
+p71xx.h
+""")
+
 libpentek = env.Library('pentek', libsources)
 
-Default(libpentek)
+html = env.Apidocs(libsources + headers, DOXYFILE_FILE = "#/Doxyfile")
+
+Default(libpentek, html)
 
 thisdir = env.Dir('.').srcnode().abspath
 def pentek(env):
