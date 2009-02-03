ee24fc73593c2453102610384b27743daf00c243
Eric Loew <ericloew@ucar.edu>
2009-02-03 21:22:31 +0000

    Added DDC filters and FPGA configuration.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/pentek@6805 da89c675-a4d7-0310-8a15-9e7546503515

--- p7142hcr.cpp
+++ p7142hcr.cpp
@@ -6,22 +6,37 @@
  */
 
 #include "p7142hcr.h"
-
-#include "p7142.h"
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <iostream>
 #include <stdio.h>
-
+#include "BuiltinGaussian.h"
+#include "BuiltinKaiser.h"
 
 using namespace Pentek;
 
 ////////////////////////////////////////////////////////////////////////////////////////
-p7142hcrdn::p7142hcrdn(std::string devName, std::string dnName, int gates, int bypdiv):
-p7142dn(devName, dnName, bypdiv),
-_gates(gates)
+p7142hcrdn::p7142hcrdn(std::string devName, std::string dnName, int gates,
+		   int delay, int prt, int prt2, int pulse_width, bool stgr_prt,
+		   std::string gaussianFile, std::string kaiserFile,int bypdivrate):
+p7142dn(devName, dnName, bypdivrate),
+_gates(gates),
+_delay(delay),
+_prt(prt),
+_pulse_width(pulse_width),
+_stgr_prt(stgr_prt),
+_gaussianFile(gaussianFile),
+_kaiserFile(kaiserFile)
+
+
 {
-	bool status = config();
+	// set up page and mask registers for FIOREGSET and FIOREGGET functions to access FPGA registers
+	_pp.page = 1;
+	_pp.mask = 0;
+
+	// configure DDC in FPGA
+	if (!config())
+		std::cout << "error initializing filters\n";
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -32,12 +47,398 @@ p7142hcrdn::~p7142hcrdn() {
 bool
 p7142hcrdn::config() {
 
-	/// program the gates
+	// stop the filters if they are running.
+	_pp.offset = KAISER_ADDR;
+	_pp.value = DDC_STOP;
+    ioctl(_dnFd, FIOREGSET, &_pp);
+	usleep(100000);
 
+	// set up the filters. Will do nothing if either of
+	// the filter file paths is empty.
+	if (filterSetup()) {
+		// error initializing the filters
+		return false;
+	}
+	// Reset Decimator clocks
+//	Adapter_Write32(&_chanAdapter, V4, DEC_RST_REG, RST_ACT);
+//	usleep(100000);
+//	Adapter_Write32(&_chanAdapter, V4, DEC_RST_REG, RST_CLR);
+//	usleep(100000);
+
+	// Start the DDC  -- do we really want to do this here???
+	_pp.offset = KAISER_ADDR;
+	_pp.value = DDC_START;
+    ioctl(_dnFd, FIOREGSET, &_pp);
+	usleep(100000);
+
+	// initialize the timers
+//	if (!timerInit())
+//		return -1;
 
 	return true;
 }
+//////////////////////////////////////////////////////////////////////
 
+bool p7142hcrdn::loadFilters(FilterSpec& gaussian, FilterSpec& kaiser) {
+
+	bool kaiserLoaded;
+	bool gaussianLoaded;
+
+	int attempt;
+
+	// program kaiser coefficients
+
+	attempt = 0;
+	do {
+		std::cout << "kaiser coeffs:\n";
+		kaiserLoaded = true;
+		for (unsigned int i = 0; i < kaiser.size(); i++) {
+			//std::cout << "   " << kaiser[i] << "\n";
+			unsigned int readBack;
+			int ramAddr = i/8;
+			int ramSelect = i%8 << 4;
+			_pp.value = 0x1000 | ramSelect | ramAddr;
+			_pp.offset = KAISER_ADDR;
+
+			// set the address
+			ioctl(_dnFd, FIOREGSET, &_pp);
+
+			// write the value
+			// LS word first
+			_pp.value = kaiser[i] & 0xFFFF;
+			_pp.offset = KAISER_DATA_LSW;
+			ioctl(_dnFd, FIOREGSET, &_pp);
+			// then the MS word -- since coefficients are 18 bits and FPGA registers are 16 bits!
+			_pp.value = (kaiser[i] >> 4) & 0x3;
+			_pp.offset = KAISER_DATA_MSW;
+			ioctl(_dnFd, FIOREGSET, &_pp);
+
+			// enable writing
+			_pp.value = 0x1;
+			_pp.offset = KAISER_WR;
+			ioctl(_dnFd, FIOREGSET, &_pp);
+
+			// disable writing (kaiser readback only succeeds if we do this)
+			_pp.value = 0x0;
+			_pp.offset = KAISER_WR;
+			ioctl(_dnFd, FIOREGSET, &_pp);
+
+			// read back the programmed value; we need to do this in two words as above.
+			_pp.offset = KAISER_READ_LSW;
+			ioctl(_dnFd, FIOREGGET, &_pp);
+			readBack = _pp.value;
+			_pp.offset = KAISER_READ_MSW;
+			ioctl(_dnFd, FIOREGGET, &_pp);
+			readBack |= _pp.value << 4;
+
+			if (readBack != kaiser[i]) {
+				std::cout << "kaiser readback failed for coefficient "
+						<< std::dec << i << std::hex << ", wrote " << kaiser[i]
+						<< ", read " << readBack << std::endl;
+
+				kaiserLoaded = false;
+			}
+		}
+		attempt++;
+	} while (!kaiserLoaded && attempt < 50);
+
+	if (kaiserLoaded) {
+		std::cout << kaiser.size()
+				<< " Kaiser filter coefficients succesfully loaded\n";
+	} else {
+		std::cout << "Unable to load the Kaiser filter coefficients\n";
+	}
+
+	// program gaussian coefficients
+	attempt = 0;
+
+	do {
+		gaussianLoaded = true;
+		//std::cout << "gaussian coeffs:\n";
+		for (unsigned int i = 0; i< gaussian.size(); i++) {
+			//std::cout << "    " << i << " " << gaussian[i] << std::endl;
+
+			unsigned int readBack;
+			int ramAddr = i%8;
+			int ramSelect = i/8 << 4;
+			_pp.value = ramSelect | ramAddr;
+			_pp.offset = GUASSIAN_ADDR;
+
+			// set the address
+			ioctl(_dnFd, FIOREGSET, &_pp);
+
+			// write the value
+			// LS word first
+			_pp.value = gaussian[i] & 0xFFFF;
+			_pp.offset = GUASSIAN_DATA_LSW;
+			ioctl(_dnFd, FIOREGSET, &_pp);
+			// then the MS word -- since coefficients are 18 bits and FPGA registers are 16 bits!
+			_pp.value = (gaussian[i] >> 4) & 0x3;
+			_pp.offset = GUASSIAN_DATA_MSW;
+			ioctl(_dnFd, FIOREGSET, &_pp);
+
+			// enable writing
+			_pp.value = 0x1;
+			_pp.offset = GUASSIAN_WR;
+			ioctl(_dnFd, FIOREGSET, &_pp);
+
+			// disable writing (kaiser readback only succeeds if we do this)
+			_pp.value = 0x0;
+			_pp.offset = GUASSIAN_WR;
+			ioctl(_dnFd, FIOREGSET, &_pp);
+
+			// read back the programmed value; we need to do this in two words as above.
+			_pp.offset = GUASSIAN_READ_LSW;
+			ioctl(_dnFd, FIOREGGET, &_pp);
+			readBack = _pp.value;
+			_pp.offset = GUASSIAN_READ_MSW;
+			ioctl(_dnFd, FIOREGGET, &_pp);
+			readBack |= _pp.value << 4;
+
+			if (readBack != gaussian[i]) {
+				std::cout << "gaussian readback failed for coefficient "
+						<< std::dec << i << std::hex << ", wrote "
+						<< gaussian[i] << ", read " << readBack << std::endl;
+
+				gaussianLoaded = false;
+			}
+		}
+		attempt++;
+	} while (!gaussianLoaded && attempt < 50);
+
+	if (gaussianLoaded) {
+		std::cout << gaussian.size()
+				<< " Gaussian filter coefficients succesfully loaded\n";
+	} else {
+		std::cout << "Unable to load the Gaussian filter coefficients\n";
+	}
+
+	return kaiserLoaded && gaussianLoaded;
+
+}
+////////////////////////////////////////////////////////////////////////
+
+int p7142hcrdn::filterSetup() {
+
+
+    // get the gaussian filter coeeficients.
+	FilterSpec gaussian;
+	if (_gaussianFile.size() != 0) {
+		FilterSpec g(_gaussianFile);
+		if (!g.ok()) {
+			std::cerr << "Incorrect or unaccesible filter definition: "
+					<< _gaussianFile << std::endl;
+			return -1;
+		} else {
+			gaussian = g;
+		}
+	} else {
+		BuiltinGaussian builtins;
+	    // The pulsewidth in microseconds. It must match one of those
+	    // available in BuiltinGaussian.
+	    double pulseWidthUs = 1.00;
+
+		// Choose the correct builtin Gaussian filter coefficient set.
+		switch (_pulse_width) {  // pulse width in 10 MHz counts
+		case 2:
+			pulseWidthUs = 0.2;
+			break;
+		case 4:
+			pulseWidthUs = 0.4;
+			break;
+		case 6:
+			pulseWidthUs = 0.6;
+			break;
+		case 8:
+			pulseWidthUs = 0.8;
+			break;
+		case 10:
+			pulseWidthUs = 1.0;
+			break;
+		case 12:
+			pulseWidthUs = 1.2;
+			break;
+		case 14:
+			pulseWidthUs = 1.4;
+			break;
+		case 16:
+			pulseWidthUs = 1.6;
+			break;
+		default:
+			std::cerr << "chip width specification of " << _pulse_width
+					<< " is not recognized, filter will be configured for a "
+					<< pulseWidthUs << " uS pulse\n";
+			break;
+		}
+		if (builtins.find(pulseWidthUs) == builtins.end()) {
+			std::cerr << "No entry for " << pulseWidthUs <<
+				" us pulsewidth in list of builtin Gaussian filters!" <<
+				std::endl;
+			abort();
+		}
+		gaussian = FilterSpec(builtins[pulseWidthUs]);
+	    std::cout << "Gaussian filter programmed for a " << pulseWidthUs << " uS pulse\n";
+	}
+
+	// get the kaiser filter coefficients
+	FilterSpec kaiser;
+	if (_kaiserFile.size() != 0) {
+		FilterSpec k(_kaiserFile);
+		if (!k.ok()) {
+			std::cerr << "Incorrect or unaccesible filter definition: "
+					<< _kaiserFile << std::endl;
+			return -1;
+		} else {
+			kaiser = k;
+		}
+	} else {
+		BuiltinKaiser k;
+		kaiser = FilterSpec(k[5.0]); // select 5 MHz bandwidth filter
+	}
+
+	std::cout << "Kaiser filter programmed for " << 5.0 << " MHz bandwidth\n";
+
+	// load the filter coefficients
+	if (!loadFilters(gaussian, kaiser)) {
+		std::cerr << "Unable to load filters\n";
+		return -1;
+	}
+
+	return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+bool p7142hcrdn::timerInit() {
+
+#ifdef USE_TIMER
+	//
+	//    This section initializes the timers.
+
+	double prtClock = (60e6); // Timer Input Clock Freq
+	int periodCount; // Period Count for all Timers
+	int PrtScheme; // PRT Scheme for all Timers
+
+	// Internal Timing Setup
+
+	unsigned int ALL_TIMERS= TIMER0 | TIMER1 | TIMER2 | TIMER3;
+
+	// Calculate the period and PRT Scheme for dual prt or single prt
+	int X, Y;
+	if (_radarParams.radd_nipp == 2) //dual prt
+	{
+		periodCount = (int) (_radarParams.radd_ipp1
+				* ((float) _radarParams.radd_ipp2 / _radarParams.radd_ipp1
+						- (int) (_radarParams.radd_ipp2
+								/ _radarParams.radd_ipp1))
+				/ (int) (_radarParams.radd_ipp2 / _radarParams.radd_ipp1)
+				* prtClock / 1e3);
+
+		X = (int) ((int) (_radarParams.radd_ipp2 / _radarParams.radd_ipp1)
+				/ ((float) _radarParams.radd_ipp2 / _radarParams.radd_ipp1
+						- (int) (_radarParams.radd_ipp2
+								/ _radarParams.radd_ipp1)));
+		Y = (int) (X * _radarParams.radd_ipp2 / _radarParams.radd_ipp1);
+
+		PrtScheme = (Y<<4) | X;
+	} else //single prt
+	{
+		periodCount = (int) ceil((_radarParams.radd_ipp1 * prtClock / 1e3));
+		PrtScheme = 0x0000;
+	}
+
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, CONTROL_REG | ALL_TIMERS); // Control Register
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, TIMER_ON); // Enable Timer
+	Adapter_Write32(&_chanAdapter, V4, MT_WR, WRITE_ON); // Turn on Write Strobes
+
+	// TIMER 0
+	// Delay Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, DELAY_REG | TIMER0); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, _radarParams.wave_chpoff[0]); // Data
+	// Pulse Width Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, WIDTH_REG | TIMER0); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, _radarParams.wave_chpwid[0]
+			* _radarParams.wave_ngates[0]); // Data
+	// TIMER 1
+	// Delay Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, DELAY_REG | TIMER1); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, _radarParams.wave_chpoff[1]); // Data
+	// Pulse Width Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, WIDTH_REG | TIMER1); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, _radarParams.wave_chpwid[1]
+			* _radarParams.wave_ngates[1]); // Data
+	// TIMER 2
+	// Delay Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, DELAY_REG | TIMER2); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, _radarParams.wave_chpoff[2]); // Data
+	// Pulse Width Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, WIDTH_REG | TIMER2); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, _radarParams.wave_chpwid[2]
+			* _radarParams.wave_ngates[2]); // Data
+	// TIMER 3
+	// Delay Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, DELAY_REG | TIMER3); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, _radarParams.wave_chpoff[3]); // Data
+	// Pulse Width Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, WIDTH_REG | TIMER3); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, _radarParams.wave_chpwid[3]
+			* _radarParams.wave_ngates[3]); // Data
+	// ALL TIMERS
+	// Period Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, PERIOD_REG | ALL_TIMERS); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, periodCount); // Data
+	//Multiple PRT Register
+	Adapter_Write32(&_chanAdapter, V4, MT_ADDR, PRT_REG | ALL_TIMERS); // Address
+	Adapter_Write32(&_chanAdapter, V4, MT_DATA, PrtScheme); // Mult PRT Value Timer 0
+
+	// Enable and Trigger All Timers
+	Adapter_Write32(&_chanAdapter, V4,
+	MT_ADDR,
+	PRT_REG | ALL_TIMERS | TIMER_EN); // Set Global Enable
+
+	usleep(1000);
+
+	Adapter_Write32(&_chanAdapter, V4, MT_WR, WRITE_OFF); // Turn off Write Strobes
+#endif
+	return true;
+
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+void p7142hcrdn::startInternalTimer() {
+#ifdef USE_TIMER
+
+	//
+	//    This start the internal timers.
+
+	unsigned int ALL_TIMERS= TIMER0 | TIMER1 | TIMER2 | TIMER3;
+
+	Adapter_Write32(&_chanAdapter, V4, MT_WR, WRITE_ON); // Turn on Write Strobes
+
+	// Enable and Trigger All Timers
+	Adapter_Write32(&_chanAdapter, V4,
+	MT_ADDR,
+	PRT_REG | ALL_TIMERS | ADDR_TRIG); // Set Global Enable
+
+	usleep(1000);
+
+	Adapter_Write32(&_chanAdapter, V4, MT_WR, WRITE_OFF); // Turn off Write Strobes
+
+	// Get current system time as xmit start time
+	setXmitStartTime(microsec_clock::universal_time());
+#endif
+}
+
+//////////////////////////////////////////////////////////////////////
+#ifdef TIME
+void p7142hcrdn::setXmitStartTime(ptime startTime) {
+	_xmitStartTime = startTime;
+}
+#endif
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
--- p7142hcr.h
+++ p7142hcr.h
@@ -9,10 +9,24 @@
 #define P7172HCR_H_
 
 #include "p7142.h"
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pthread.h>
+
+#include <string>
+#include <vector>
+#include <deque>
+#include <map>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "FilterSpec.h"
+#include "DDCregisters.h"
 
 namespace Pentek {
 
-	/// A p7142 downconvertor for use with the hcr firmware
+	/// A p7142 downconvertor for use with the HCR firmware
 	class p7142hcrdn: public p7142dn {
 		public:
 			/// Constructor
@@ -20,17 +34,71 @@ namespace Pentek {
 			/// /dev/pentek/p7140/0.
 			/// @param dnName The name of the downconvertor device, e.g. 0B
 			/// @param gates The number of gates
+			/// @param delay the delay to the first gate in 10 MHz counts
+			/// @param prt the radar PRT in 10 MHz counts
+			/// @param prt2 the second PRT of a staggered PRT sequence, expressed in 10 MHz counts
+			/// @param pulse_width the radar pulse width in 10 MHz counts
+			/// @param stgr_prt the staggered PRT flag; 1 = stagger, 0 = uniform
 			/// @bypassdivrate The byopass divider (decimation) rate
-			p7142hcrdn(std::string devName, std::string dnName, int gates, int bypassdivrate=1);
+			p7142hcrdn(std::string devName, std::string dnName, int gates, int delay, int prt,
+					   int prt2, int pulse_width, bool stgr_prt,
+					   std::string gaussianFile, std::string kaiserFile,
+					   int bypassdivrate=1);
 			/// Destructor
 			virtual ~p7142hcrdn();
 
 		protected:
 			/// Configure the p7142hcrdn
-			/// @return True if the configuration was succesful
+			/// @return True if the configuration was successful
+
 			bool config();
+
+			/// Configure the filters and the decimation value.
+	        int filterSetup();
+
+	        /// Program the cofficients for the gaussian and
+	        /// kaiser filters.
+	        bool loadFilters(FilterSpec& gaussian,FilterSpec& kaiser);
+
+	        /// Configure the timers.
+	        /// @return true if successful, false otherwise.
+	        bool timerInit();
+
+	        /// Starts the timers.
+	         void startInternalTimer();
+
+	         /// Set the time of the first transmit pulse.
+	         /// @param startTime The boost::posix_time::ptime of the first transmit
+	         ///    pulse.
+	         void setXmitStartTime(boost::posix_time::ptime startTime);
+
+	         /// Time of first transmit pulse
+	         boost::posix_time::ptime xmitStartTime()
+	         {
+	             return _xmitStartTime;
+	         }
+
 			/// number of gates
 			int _gates;
+			/// radar PRT in 10 MHz counts
+			int _delay;
+			/// second PRT of staggered PRT in 10 MHz counts
+			int _prt;
+			/// delay to first gate in 10 MHz counts
+			int _prt2;
+			/// radar pulse width in 10 MHz counts
+			int _pulse_width;
+			/// staggered PRT flag: 1 = stagger, 0 = uniform
+			bool _stgr_prt;
+	        /// The path to the file containing the gaussian filter definitions.
+	        std::string _gaussianFile;
+	        /// The path to the file containing the kaiser filter coefficients.
+	        std::string _kaiserFile;
+	        /// Time of the first xmit pulse.
+	         boost::posix_time::ptime _xmitStartTime;
+	        ///
+	         ARG_PEEKPOKE _pp;
+
 	};
 
 }
