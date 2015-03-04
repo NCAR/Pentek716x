b5c5783a0c6060928f7a9f71ed455889243b2cde
Chris Burghart <burghart@ucar.edu>
2015-03-04 19:58:49 +0000

    Explicitly set triggerMode = P716x_ADC_GATE_TRIG_CTRL_TRIG_MODE_GATE, even though it's the default. It makes it clear that the gate signal is being used as the trigger.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/branches/pentek_716x@9224 da89c675-a4d7-0310-8a15-9e7546503515

--- p716xDn_sd3c.cpp
+++ p716xDn_sd3c.cpp
@@ -207,6 +207,7 @@ p716xDn_sd3c::_setupAdcParams(P716x_ADC_CHAN_PARAMS & adcChanParams) {
     adcChanParams.dataPackMode = P716x_ADC_DATA_CTRL_PACK_MODE_IQ_DATA_PACK;
 
     // Gate trigger is used to start our timers
+    adcChanParams.triggerMode = P716x_ADC_GATE_TRIG_CTRL_TRIG_MODE_GATE;
     adcChanParams.gateTrigEnable = P716x_ADC_GATE_TRIG_CTRL_GATE_TRIG_IN_ENABLE;
 }
 
