56d704c4cf60dc27b4fd097bb6cf8a56dca204bd
Chris Burghart <burghart@ucar.edu>
2015-04-02 20:24:50 +0000

    Added program sd3cInfo to print SD3C DDC type and revision number read from a Pentek card.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/branches/pentek_716x@9299 da89c675-a4d7-0310-8a15-9e7546503515

---
+++ test/sd3cInfo.cpp
@@ -0,0 +1,76 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <boost/program_options.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <csignal>
+#include <p716x_sd3c.h>
+
+using namespace std;
+using namespace boost::posix_time;
+namespace po = boost::program_options;
+
+//////////////////////////////////////////////////////////////////////
+//
+/// Parse the command line options, and also set some options
+/// that are not specified on the command line.
+/// @return The runtime options that can be passed to the
+/// threads that interact with the RR314.
+
+void parseOptions(int argc, char** argv)
+{
+
+    // get the options
+    po::options_description descripts("Options");
+    descripts.add_options()
+            ("help", "Print the help message and exit")
+            ;
+
+    po::variables_map vm;
+    po::command_line_parser parser(argc, argv);
+    po::positional_options_description pd;
+    po::store(parser.options(descripts).positional(pd).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        cout << "Print the SD3C DDC type and bitstream revision number " <<
+                "installed on a Pentek 716x card";
+        cout << "Usage: " << argv[0];
+        cout << descripts << endl;
+        exit(0);
+    }
+
+}
+
+///////////////////////////////////////////////////////////
+int
+main(int argc, char** argv)
+{
+    // parse the command line options, substituting for config params.
+    parseOptions(argc, argv);
+
+    // set to ignore SIGPIPE errors which occur when sockets
+    // are broken between client and server
+    signal(SIGPIPE, SIG_IGN);
+
+    // Initialize the library
+
+    DWORD dwStatus = PTK716X_LibInit();
+    if (PTK716X_STATUS_OK != dwStatus) {
+        cerr << "Failed to initialize PTK716X library!" << endl;
+        return(1);
+    }
+
+    // Find and open the PTK716x device
+    // user will be asked to pick the device num
+
+    Pentek::p716x_sd3c sd3cCard(100.0e6, true, true, false, 10.0, 
+                                Pentek::p716x_sd3c::DDC10DECIMATE,
+                                0.0, 1.0e-6, 1000, 0, false, 100, 0, false,
+                                false, false, 0);
+    cout << "Card has SD3C " << sd3cCard.ddcTypeName() << 
+            " bitstream, revision " << sd3cCard.sd3cRev();
+
+    return(0);
+}
+
